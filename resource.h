080a423a2d0a303e7e5448de44d5ef90a3aeca29
sarath4564 <sarathchandrakaja@gmail.com>
2022-02-06 15:37:10 +0530

    Add project files.

---
+++ resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DialogBox.rc
+//
+#define IDD_DIALOGBOX_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_CHECK1                      1002
+#define IDC_LIST1                       1003
+#define IDC_LIST2                       1004
+#define IDC_RADIO1                      1005
+#define IDC_RADIO2                      1006
+#define IDC_BUTTON1                     1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
